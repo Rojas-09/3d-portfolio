2835a3b4966752769f697a1a19585f8833462666
Rojas-09 <juan.rojas7@utp.edu.co>
2026-05-30 01:06:28 -0500

    feat: rebrand portfolio to Juan Andrés Rojas

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -76,14 +76,14 @@ const HeroSection = () => {
                       "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
                     )}
                   >
-                    A Full Stack Web Developer
+                    A Software Engineer
                   </p>
                 </BlurIn>
               </div>
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1MTSsUA8V7Po2AsNXT8kZ5sLOpzC8l7qm/view?usp=sharing"
+                    "https://drive.google.com/file/d/1TzKUg8aAXPcwalwtRiFRHGnis5dzYy8c/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
